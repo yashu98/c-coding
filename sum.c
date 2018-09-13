a38e20e32fc6234d5c532da0718ebd46e311fe95
yashu98 <42237378+yashu98@users.noreply.github.com>
2018-09-13 14:25:33 +0530

    sum

---
+++ sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,sum=0;
+  printf("enter the numbers to be added\n");
+  scanf("%d%d",&a,&b);
+  sum=a+b;
+  printf("the sum of %d and %d is %d",a,b,sum);
+}
